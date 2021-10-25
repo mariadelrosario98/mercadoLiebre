35ba8645282ea23ddb500a0470c41193ad11e46b
mariadelrosario98 <md.castro@uniandes.edu.co>
2021-10-24 22:27:02 -0500

    Mercado Liebre final

--- app.js
+++ app.js
@@ -10,8 +10,12 @@ app.listen(3000,() => {
   console.log("servidor corriendo en puerto 3000")
 });
 
-
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/home.html"));
 })
 
+app.get('/register.html', (req, res) => {
+  res.sendFile(path.resolve(__dirname,'./views/register.html'))});
+
+app.get('/login.html', (req, res) => {
+  res.sendFile(path.resolve(__dirname,'./views/login.html'))});
