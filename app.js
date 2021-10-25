e8f1ea6141df74d0f8e5158a48f25cd69c1e53e9
mariadelrosario98 <md.castro@uniandes.edu.co>
2021-10-24 23:10:29 -0500

    actualizacion app.js

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const publicPath = path.resolve(__dirname, "./public")
+const publicPath = path.resolve(__dirname, "./public");
+const PORT = process.env.PORT || 3000;
 
 //Para configurar nuestra carpeta de archivos públicos
 app.use(express.static(publicPath));
 
-app.listen(process.env.PORT||3000,function(){
+app.listen(PORT,()=>{
   console.log("servidor corriendo en puerto 3000")
-});
+})
+
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/home.html"));
